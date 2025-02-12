b1870719e800023689173d1de5c03e8f4179ed8c
MarceloHabreu <marcelohenriqueas17@gmail.com>
2025-02-12 16:53:30 -0300

    feat: user crud, first version finished!

--- src/main/java/io/github/marcelohabreu/bookManagement/controllers/UserController.java
+++ src/main/java/io/github/marcelohabreu/bookManagement/controllers/UserController.java
@@ -4,10 +4,9 @@ import io.github.marcelohabreu.bookManagement.dtos.UserDTO;
 import io.github.marcelohabreu.bookManagement.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/bookmanagement")
@@ -17,7 +16,27 @@ public class UserController {
     private UserService service;
 
     @PostMapping
-    public ResponseEntity<?> create(@RequestBody UserDTO u){
+    public ResponseEntity<?> create(@RequestBody UserDTO u) {
         return service.saveUser(u);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDTO> getUser(@PathVariable Long id) {
+        return service.getUserById(id);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UserDTO>> list() {
+        return service.listAllUsers();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<String> update(@RequestBody UserDTO u, @PathVariable Long id) {
+        return service.updateUser(u, id);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> delete(@PathVariable Long id) {
+        return service.deleteUser(id);
     }
 }
--- src/main/java/io/github/marcelohabreu/bookManagement/services/UserService.java
+++ src/main/java/io/github/marcelohabreu/bookManagement/services/UserService.java
@@ -2,13 +2,16 @@ package io.github.marcelohabreu.bookManagement.services;
 
 import io.github.marcelohabreu.bookManagement.dtos.UserDTO;
 import io.github.marcelohabreu.bookManagement.exceptions.EmailAlreadyExistsException;
+import io.github.marcelohabreu.bookManagement.exceptions.UserNotFoundException;
 import io.github.marcelohabreu.bookManagement.models.User;
 import io.github.marcelohabreu.bookManagement.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -17,14 +20,59 @@ public class UserService {
     @Autowired
     private UserRepository repository;
 
-    public ResponseEntity<?> saveUser(UserDTO u){
-        User newUser = u.toModel();
-        Optional<User> emailAlreadyExists = repository.findByEmail(newUser.getEmail());
-        if (emailAlreadyExists.isPresent()){
+    private void checkEmail(User u, Long id) {
+        Optional<User> emailAlreadyExists = repository.findByEmail(u.getEmail());
+        if (emailAlreadyExists.isPresent() && !emailAlreadyExists.get().getId().equals(id)) {
             throw new EmailAlreadyExistsException("The e-mail you are trying to register already exists, try again!");
         }
+    }
+
+    @Transactional
+    public ResponseEntity<String> saveUser(UserDTO u) {
+        User newUser = u.toModel();
+        checkEmail(newUser, newUser.getId());
 
         repository.save(newUser);
-        return ResponseEntity.status(HttpStatus.CREATED).body("User successfully registered");
+        return ResponseEntity.status(HttpStatus.CREATED).body("User successfully registered.");
+    }
+
+    public ResponseEntity<UserDTO> getUserById(Long id) {
+        return repository.findById(id)
+                .map(UserDTO::fromModel)
+                .map(ResponseEntity::ok)
+                .orElseThrow(() -> new UserNotFoundException("User not found, try again."));
+    }
+
+    public ResponseEntity<List<UserDTO>> listAllUsers() {
+        List<UserDTO> users = repository.findAll().stream().map(UserDTO::fromModel).toList();
+        return ResponseEntity.ok(users);
+    }
+
+    @Transactional
+    public ResponseEntity<String> updateUser(UserDTO u, Long id) {
+        Optional<User> userExists = repository.findById(id);
+
+        if (userExists.isEmpty()) {
+            throw new UserNotFoundException("User Not found, try again!");
+        }
+
+        User userUpdated = u.toModel();
+        userUpdated.setId(id);
+        userUpdated.setName(u.name());
+        userUpdated.setEmail(u.email());
+
+        checkEmail(userUpdated, id);
+
+        repository.save(userUpdated);
+        return ResponseEntity.status(HttpStatus.OK).body("User successfully updated.");
+
+    }
+
+    public ResponseEntity<String> deleteUser(Long id) {
+        return repository.findById(id)
+                .map(user -> {
+                    repository.delete(user);
+                    return ResponseEntity.status(HttpStatus.OK).body("User successfully deleted.");
+                }).orElseThrow(() -> new UserNotFoundException("User not found, try again!"));
     }
 }
