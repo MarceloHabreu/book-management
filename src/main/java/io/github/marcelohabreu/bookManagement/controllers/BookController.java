fea1da0a17b3b434260043a883656fbe0062bbaf
MarceloHabreu <marcelohenriqueas17@gmail.com>
2025-02-14 17:10:41 -0300

    feat: book crud, first version finished

--- src/main/java/io/github/marcelohabreu/bookManagement/controllers/BookController.java
+++ src/main/java/io/github/marcelohabreu/bookManagement/controllers/BookController.java
@@ -4,10 +4,9 @@ import io.github.marcelohabreu.bookManagement.dtos.BookDTO;
 import io.github.marcelohabreu.bookManagement.services.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/bookmanagement/books")
@@ -19,5 +18,25 @@ public class BookController {
     @PostMapping
     public ResponseEntity<String> create(@RequestBody BookDTO b){
         return service.saveBook(b);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<BookDTO> getBook(@PathVariable Long id){
+        return service.getBookById(id);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<BookDTO>> getBook(){
+        return service.listAllBooks();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<String> update(@RequestBody BookDTO b, @PathVariable Long id){
+        return service.updateBook(b, id);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> delete(@PathVariable Long id) {
+        return service.deleteBook(id);
     }
 }
--- src/main/java/io/github/marcelohabreu/bookManagement/services/BookService.java
+++ src/main/java/io/github/marcelohabreu/bookManagement/services/BookService.java
@@ -2,6 +2,7 @@ package io.github.marcelohabreu.bookManagement.services;
 
 import io.github.marcelohabreu.bookManagement.dtos.BookDTO;
 import io.github.marcelohabreu.bookManagement.exceptions.BookAlreadyExistException;
+import io.github.marcelohabreu.bookManagement.exceptions.BookNotFoundException;
 import io.github.marcelohabreu.bookManagement.models.Book;
 import io.github.marcelohabreu.bookManagement.repositories.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Comparator;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -20,7 +23,7 @@ public class BookService {
 
     private void checkBook(Book b){
         Optional<Book> bookExists = repository.findByTitleAndAuthor(b.getTitle(), b.getAuthor());
-        if (bookExists.isPresent()){
+        if (bookExists.isPresent() && !bookExists.get().getId().equals(b.getId())){
             throw new BookAlreadyExistException("This book already exists!");
         }
     }
@@ -32,5 +35,46 @@ public class BookService {
 
         repository.save(newBook);
         return ResponseEntity.status(HttpStatus.CREATED).body("Book successfully registered.");
+    }
+
+    public ResponseEntity<BookDTO> getBookById(Long id){
+        return repository.findById(id)
+                .map(BookDTO::fromModel)
+                .map(ResponseEntity::ok)
+                .orElseThrow(() -> new BookNotFoundException("Book not found! Try again."));
+    }
+
+    public ResponseEntity<List<BookDTO>> listAllBooks(){
+        List<BookDTO> allBooks = repository.findAll().stream().sorted(Comparator.comparing(Book::getId)).map(BookDTO::fromModel).toList();
+        return ResponseEntity.ok(allBooks);
+    }
+
+    @Transactional
+    public ResponseEntity<String> updateBook(BookDTO b, Long id){
+        Optional<Book> bookExists = repository.findById(id);
+
+        if (bookExists.isEmpty()){
+            throw new BookNotFoundException("Book not found! Try again.");
+        }
+
+        Book bookUpdated = b.toModel();
+        bookUpdated.setId(id);
+        bookUpdated.setTitle(b.title());
+        bookUpdated.setAuthor(b.author());
+        bookUpdated.setBorrowed(b.isBorrowed());
+        bookUpdated.setCreated_at(bookExists.get().getCreated_at());
+
+        checkBook(bookUpdated);
+
+        repository.save(bookUpdated);
+        return ResponseEntity.status(HttpStatus.OK).body("Book successfully updated.");
+    }
+
+    public ResponseEntity<String> deleteBook(Long id) {
+        return repository.findById(id)
+                .map(book -> {
+                    repository.delete(book);
+                    return ResponseEntity.status(HttpStatus.OK).body("Book successfully deleted.");
+                }).orElseThrow(() -> new BookNotFoundException("Book not found! Try again."));
     }
 }
