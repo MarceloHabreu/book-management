302180ec26c067eed26bc532132bf632b3cbe017
MarceloHabreu <marcelohenriqueas17@gmail.com>
2025-03-09 16:21:01 -0300

    🔄 refactor : Modifying YML properties and resource visibility with variables in the .env file.

--- bookmanagement-back/src/main/java/io/github/marcelohabreu/bookManagement/BookManagementApplication.java
+++ bookmanagement-back/src/main/java/io/github/marcelohabreu/bookManagement/BookManagementApplication.java
@@ -1,5 +1,6 @@
 package io.github.marcelohabreu.bookManagement;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -9,6 +10,13 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class BookManagementApplication {
 
     public static void main(String[] args) {
+        // Carrega o arquivo .env
+        Dotenv dotenv = Dotenv.configure()
+                .directory("./") // Diretório onde o .env está (raiz do projeto)
+                .load();
+
+        // Define as variáveis como propriedades do sistema
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
         SpringApplication.run(BookManagementApplication.class, args);
     }
 
